2685bb8aa254af056a153378af6db98757277a9a
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-01-04 18:02:11 -0600

    allger

--- train/G_E_R.py
+++ train/G_E_R.py
@@ -6,13 +6,13 @@ Created on Wed Sep 15 22:12:11 2021
 @author: yigongqin
 """
 
-batch = 15
-num_batch = 100
+batch = 4
+num_batch = 1000
 num_runs = batch*num_batch
-valid_ratio = 1/3
+valid_ratio = 1/4
 num_train_all = int((1-valid_ratio)*num_runs)
 num_test = num_runs-num_train_all
-num_train = num_batch*5 #num_train_all
+num_train = num_batch*1 #num_train_all
 num_train_b = int(num_train_all/num_batch)
 num_test_b = int(num_test/num_batch)
 
@@ -27,7 +27,7 @@ dt = 1.0/(frames-1)
 
 G = 8     # G is the number of grains
 G_small = 8
-param_len = G + 3   # how many parameters, color plus 3 physical
+param_len = G + 4   # how many parameters, color plus 3 physical
 output_len = G
 
 ## architecture
@@ -45,6 +45,7 @@ seed = 1
 #data_dir = '../../fixed_size/*G01.*'
 data_dir = '../../final_ge/*'
 data_dir = '../../large_R/*'
+data_dir = '../../allgr/*'
 #data_dir = '../../fixed_size/*'
 #data_dir = '../../all_qoi/*'
 #data_dir = '../../ML_PF8_train1000_test100_Mt24002_grains8_frames25_anis0.050_G050.000_Rmax1.000_seed0_rank0.h5'
