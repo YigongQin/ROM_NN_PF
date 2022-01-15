303be512c199a315881f909c3fd787a9ae648728
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-01-15 11:14:01 -0600

    area

--- train/plot_funcs.py
+++ train/plot_funcs.py
@@ -219,7 +219,7 @@ def miss_rate(anis,G0,Rmax,G,x,y,aseq,tip_y,alpha_true,frac,window,plot_idx,ymax
 
     ## count for the error of area
     for g in range(G):
-      miss += np.absolute(area-area_true)
+      miss += np.absolute(area[g]-area_true[g])
 
     miss_rate = miss/( nx*nymax + np.sum(area_true) );
  
