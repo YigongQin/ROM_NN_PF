6a9223be63299f8da51de6619114c7f344d61495
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-01-18 20:35:07 -0600

    split

--- train/split_merge.py
+++ train/split_merge.py
@@ -55,19 +55,28 @@ def split_grain(param_dat, seq_dat, G, G_all):
             frac_sliced =  G_all/G*seq_dat[:,:,2*i:G+2*i]  ## frac
             dfrac_sliced = G_all/G*seq_dat[:,:,2*i+G_all:G+2*i+G_all] # dfrac
             darea_sliced = seq_dat[:,:,2*i+2*G_all:G+2*i+2*G_all]
-            '''
+            
+            if i==expand-1:
+
+                param_sliced = np.flip(param_sliced, axis = -1)
+                frac_sliced = np.flip(frac_sliced, axis = -1)
+                dfrac_sliced = np.flip(dfrac_sliced, axis = -1)
             ## here requires cut/add to make sure unity
             fsum = np.cumsum(frac_sliced, axis=-1)
             psum = np.cumsum(param_sliced, axis=-1)
 
-            
-            frac_sliced -= np.diff((fsum>1)*(fsum-1),axis=-1,prepend=0)
-         
+            frac_sliced -= np.diff((fsum>1)*(fsum-1),axis=-1,prepend=0)         
             param_sliced -= np.diff((psum>1)*(psum-1),axis=-1,prepend=0)
 
             frac_sliced[:,:,-1] = ones - np.sum(frac_sliced[:,:,:-1], axis=-1)
             dfrac_sliced[:,:,-1] = zeros - np.sum(dfrac_sliced[:,:,:-1], axis=-1)
             param_sliced[:,-1] = ones_p - np.sum(param_sliced[:,:-1], axis=-1)
+
+            if i==expand-1:
+
+                param_sliced = np.flip(param_sliced, axis = -1)
+                frac_sliced = np.flip(frac_sliced, axis = -1)
+                dfrac_sliced = np.flip(dfrac_sliced, axis = -1)
             '''
             diff_frac = ones - np.sum(frac_sliced, axis=-1)
             diff_param = ones_p - np.sum(param_sliced, axis=-1)
@@ -93,7 +102,7 @@ def split_grain(param_dat, seq_dat, G, G_all):
                 frac_sliced[:,:,0] += diff_frac/2
                 param_sliced[:,0] += diff_param/2
                 dfrac_sliced[:,:,0] += diff_dfrac/2
-
+            '''
             assert np.linalg.norm( np.sum(param_sliced,axis=-1) - ones_p ) <1e-5
             assert np.linalg.norm( np.sum(frac_sliced,axis=-1) - ones ) <1e-5
             assert np.linalg.norm( np.sum(dfrac_sliced,axis=-1) - zeros ) <1e-5
