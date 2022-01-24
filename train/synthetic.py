e37e2afaa9b7e8936c1172eb5c8e0344f02d664b
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-01-24 11:37:51 -0600

    synthetic

--- train/synthetic.py
+++ train/synthetic.py
@@ -82,7 +82,7 @@ frac[:,-1] = np.ones(evolve_runs) - np.sum(frac[:,:-1], axis=-1)
 
 
 print('sample frac', frac[0,:])
-print('sample param', param_data[0,:])
+print('sample param', param_dat[0,:])
 assert np.linalg.norm( np.sum(frac, axis=-1) - np.ones(evolve_runs) ) <1e-5
 
 param_dat[:,:G] = frac
