545cac16a65418bda4bb4a69324aa1e11097ada9
Yigong Qin <yigongqin@wireless-10-145-114-244.public.utexas.edu>
2022-01-27 16:50:30 -0600

    plot

--- train/plot_funcs.py
+++ train/plot_funcs.py
@@ -268,8 +268,7 @@ def plot_synthetic(anis,G0,Rmax,G,x,y,aseq,tip_y, frac, plot_idx,final,pf_angles
       ax3 = fig.add_subplot(111)
       cs3 = ax3.imshow(pf_angles[field].T,cmap=plt.get_cmap('jet'),origin='lower',extent= (xmin,xmax, ymin, ytop))
       subplot_rountine(fig, ax3, cs3, 3)
-      ax3.set_title('final:NN_predict_'+str(int(miss_rate*100))+'%error', color=bg_color, fontsize=8)
-      
+    
       plt.savefig(var + '_grains' + str(G) + '_case' + str(plot_idx) +'.png',dpi=800,facecolor="white", bbox_inches='tight')
       plt.close()
 
