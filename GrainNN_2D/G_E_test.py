57c78f88ff1278cca3a0f1220ed6fd7c54f2c101
Yigong Qin <yigongqin@Yigongs-MBP.lan>
2022-06-19 10:34:05 -0500

    32 as benchmarg

--- GrainNN_2D/G_E_test.py
+++ GrainNN_2D/G_E_test.py
@@ -7,28 +7,27 @@ Created on Wed Sep 15 22:12:11 2021
 """
 
 batch = 1
-num_batch = 100
-num_batch_test = 100
+num_batch = 1
+num_batch_test = 1
 num_runs = batch*num_batch
 valid_ratio = 1
 num_train_all = int((1-valid_ratio)*num_runs)
 num_test = num_runs #-num_train_all
 num_train = num_runs #num_train_all
 num_train_b = int(num_train_all/num_batch)
-num_test_b = int(num_test/num_batch)
 
 dilation = 1
 out_win = 4
 window = out_win
 frames = 20*dilation +1
-all_frames = 600*dilation +1
-train_frames=frames
+all_frames = 20*dilation +1
+inf_frames=frames
 pred_frames= frames-window
 sam_per_run = frames - window - (out_win-1)
 total_size = frames*num_runs
 dt = dilation*1.0/(frames-1)
 
-G = 8    # G is the number of grains
+G = 32    # G is the number of grains
 G_small = 8
 param_len = G + 4   # how many parameters, color plus 3 physical
 output_len = G
@@ -52,6 +51,8 @@ size_scale = 1
 #valid_dir = '../../validation/*.h5'
 #data_dir = '../../double_grains/*.h5'
 #data_dir = '../../quadra_grains/*.h5'
-data_dir = '../../testing/*.h5'
+model_dir = './fecr_model/'
+#data_dir = './testing_Plot/ML_PF8_train0_test1_grains8_frames20_anis0.080_G02.400_Rmax1.520_seed6933302_rank0_grainsize2.500_Mt102160.h5'
+data_dir = './plot_dat/ML_PF32_train0_test1_grains32_frames600_anis0.080_G02.400_Rmax1.520_seed6933304_rank0_grainsize2.500_Mt102000.h5'
 valid_dir = data_dir
 skip_check = False
